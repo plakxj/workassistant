7853184048e46db82c16f1381d456f6071abeaea
start <39118555+plakxj@users.noreply.github.com>
2019-12-11 17:12:30 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+# -*- coding: utf-8-*-
 
+from flask import Flask,render_template
 
-app = Flask（）
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 
