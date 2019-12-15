2536e67819725262a425642201dddcd743ae02f8
天使彦 <346020470@qq.com>
2019-12-15 22:26:55 +0800

    hello world success!

--- work/views.py
+++ work/views.py
@@ -1,6 +1,9 @@
 from work.forms import RecordForm
 from flask import flash,redirect,url_for,render_template
-@app.route("/form")
+from work import app
+
+@app.route("/")
 def recordform():
     form = RecordForm()
-    return render_template("form.html",form=form)
+    #return render_template("form.html",form=form)
+    return "hello world"
