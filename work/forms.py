e70ce6e25902b1ba5c3d0564c2ef7fefd33fc0fb
plakxj <39118555+plakxj@users.noreply.github.com>
2019-12-27 17:52:28 +0800

    add function delete

--- work/forms.py
+++ work/forms.py
@@ -8,3 +8,20 @@ class RecordForm(FlaskForm):
     keyword2 = StringField("keyword2", validators=[DataRequired()])
     text = StringField("text", validators=[DataRequired()])
     submit = SubmitField("save")
+
+class ShowForm(FlaskForm):
+    subject = StringField("subject",validators=[DataRequired()])
+    keyword1 = StringField("keyword1",validators=[DataRequired()])
+    keyword2 = StringField("keyword2", validators=[DataRequired()])
+    text = StringField("text", validators=[DataRequired()])
+    save = SubmitField("save")
+
+class EditForm(FlaskForm):
+    subject = StringField("subject",validators=[DataRequired()])
+    keyword1 = StringField("keyword1",validators=[DataRequired()])
+    keyword2 = StringField("keyword2", validators=[DataRequired()])
+    text = StringField("text", validators=[DataRequired()])
+    update = SubmitField("update")
+
+class DeleteForm(FlaskForm):
+    delete = SubmitField("delete")
--- work/views.py
+++ work/views.py
@@ -1,12 +1,12 @@
-
+﻿
 from flask import flash,redirect,url_for,render_template
 from work import app,db
-from work.forms import RecordForm
+from work.forms import RecordForm,EditForm,DeleteForm
 from work.models import Note
 
 import click
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/recordform",methods=["GET","POST"])
 def recordform():
     form = RecordForm()
     if form.validate_on_submit():
@@ -19,13 +19,46 @@ def recordform():
         db.session.commit()
         flash("you text is saved")
         redirect(url_for("showform"))
-    return render_template("form.html",form=form)
+    return render_template("recordform.html",form=form)
 
-@app.route("/showform",methods=["GET","POST"])
+
+@app.route("/showform",methods=["GET"])
 def showform():
+    form = DeleteForm()
     notes = Note.query.all()
-    return render_template("showform.html",notes=notes)
+    return render_template("showform.html", notes=notes, form=form)
 
+
+@app.route("/delete/<note_id>",methods=["POST"])
+def deleteform(note_id):
+    form = DeleteForm()
+    if form.validate_on_submit():
+        note = Note.query.get(note_id)
+        db.session.delete(note)
+        db.session.commit()
+        flash("当前笔记已删除")
+    else:
+        abort(400)
+    return redirect(url_for("showform"))
+
+
+@app.route("/editform/<note_id>",methods=["GET","POST"])
+def editform(note_id):
+    form = EditForm()
+    note = Note.query.get(note_id)
+    if form.validate_on_submit():
+        note.subject = form.subject.data
+        note.keyword1 = form.keyword1.data
+        note.keyword2 = form.keyword2.data
+        note.text = form.text.data
+        db.session.commit()
+        flash("笔记已更新")
+        redirect(url_for("showform"))
+    form.subject.data = note.subject
+    form.keyword1.date = note.keyword1
+    form.keyword2.data = note.keyword2
+    form.text.data = note.text
+    return render_template("editform.html",form=form)
 
 @app.cli.command()
 @click.option('--drop', is_flag=True, help='Create after drop.') 
