4d168ea0fa30ff79bc5206b1dc682554b4543b25
plakxj <39118555+plakxj@users.noreply.github.com>
2020-02-22 12:05:52 +0800

    css change

--- work/settings.py
+++ work/settings.py
@@ -3,7 +3,7 @@
 import os
 
 
-dev_db = "sqlite:///" + os.path.join("D:\PycharmProjects\workassistant","data.db")
+dev_db = "sqlite:///" + os.path.join("F:\work","data.db")
 
 
 SECRET_KEY = os.getenv("SECRET_KEY","secret string")
